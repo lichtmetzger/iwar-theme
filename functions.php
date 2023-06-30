01f01eb8a6af6a2d9dd884652764bf2a17d9ae82
Danny Schmarsel <dsc@qbus.de>
2023-06-30 16:06:55 +0200

    Add docblock for autoloader

--- functions.php
+++ functions.php
@@ -1,11 +1,13 @@
 <?php
-/*
+/**
+ *
  * GeneratePress child theme functions and definitions.
  *
  * @package iwar-theme
  * @author Danny Schmarsel <ds@lichtmetzger.de>
  */
 
+// Include PSR4-compatible autoloader.
 require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 
 // Initialize theme.
